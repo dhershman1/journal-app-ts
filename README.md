07af9db6860a332432ef84e01da9f26f2a36ee66
dhershman1 <dustinh17@gmail.com>
2024-04-07 22:38:48 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Journal App
+
+This is a journal application built using TypeScript from the vue schools course:
+https://vueschool.io/courses/typescript-with-vue-js-3
+
+## How to Install
+```sh
+npm install
+npm run dev
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import TheHeader from '@/components/TheHeader.vue'
+import EntryEditor from './components/EntryEditor.vue'
+import EntryCard from '@/components/EntryCard.vue'
+import User from './types/User'
+
+const user: User = reactive({
+  id: 1,
+  username: 'dhershman_io',
+  settings: []
+})
+
+console.log(user)
+</script>
+
+<template>
+  <main class="container m-auto p-10">
+    <TheHeader />
+    <EntryEditor />
+    <ul>
+      <li>
+        <EntryCard />
+      </li>
+    </ul>
+  </main>
+</template>
